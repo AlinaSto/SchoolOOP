92b39b6d629d0f5f9b9646e3950a55ff2cc27a40
Alinasto <alinaoprea05@yahoo.com>
2022-07-17 16:02:30 +0300

    Student&Teacher

---
+++ SchoolGenerate.java
@@ -0,0 +1,11 @@
+package SchoollOOP;
+
+public class SchoolGenerate {
+    public static void main(String[] args) {
+        Teacher Ann = new Teacher("Ann ", " Johannes ", " dancer ", " Cha-Cha.");
+        Ann.introduce();
+        Student Alex = new Student("Alex ", " Dorob ", " 8C.");
+        Alex.introduce();
+
+    }
+}
---
+++ Student.java
@@ -0,0 +1,23 @@
+package SchoollOOP;
+
+public class Student extends Person{
+    private String groupClass;
+
+    public Student(String firstName, String lastName, String groupClass) {
+        super(firstName, lastName);
+        this.groupClass = groupClass;
+    }
+
+    public String getGroupClass() {
+        return groupClass;
+    }
+
+    public void setGroupClass(String groupClass) {
+        this.groupClass = groupClass;
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("I am " +this.getFirstName()+ this.getLastName()+ ", I am in  " +this.groupClass+ " and I am a student");
+    }
+}
